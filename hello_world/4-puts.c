f5fcb878b45c2dbb46d298c1a4fb6e4f685ed6d9
Nicole222A <nicoledelosreyes2722@gmail.com>
2023-06-15 14:03:16 -0700

    4. Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  Write a C program that prints a message
+ * followed by a new line
+ * Return: always 0
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
