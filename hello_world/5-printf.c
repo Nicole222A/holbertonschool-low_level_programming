6fae13bec195c360d9ef0884b5f1e38b81d1783f
Nicole222A <nicoledelosreyes2722@gmail.com>
2023-06-16 05:43:04 -0700

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints exactly with
+ *        proper grammar, but the outcome is a piece of art,
+ *        followed by a new line.
+ * Return: always 0
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
